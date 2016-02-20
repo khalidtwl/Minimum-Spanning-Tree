ba0e1de33eec3d46143c328f22ea71c1a9baf63a
Mick Kajornrattana <ckajornrattana@college.harvard.edu>
2016-02-20 16:05:42 -0500

    merge

--- randmst.java
+++ randmst.java
@@ -23,7 +23,7 @@ class randmst {
             System.out.println();
         }
 
-<<<<<<< HEAD
+        // Prim's Algorithm
         MinHeap heap = new MinHeap(numpoints);         // initialize MinHeap of size numpoints
         boolean[] isInMST = new boolean[numpoints];    // if v[i] is true, vertex i has been included in MST
         
@@ -32,28 +32,11 @@ class randmst {
             System.out.println("(" + v + "," + heap.u + ")  "+  heap.min_dist);
             isInMST[v] = true;
             for (int i = 0; i < numpoints; i++) {
-                if (!isInMST[i] && c.weight(v,i) < heap.dist[i]) {
+                if (!isInMST[i] && c.weight(v,i) < heap.dist[i] && c.weight(v,i) > 0) {
                     heap.change(i, c.weight(v,i), v);
-=======
-        // Prim's Algorithm
-        MinHeap h = new MinHeap(numpoints);
-        boolean[] isInTree = new boolean[numpoints];
-        int v = -1;
-        int n = numpoints;
-        while(n != 0) {
-            h.deletemin();
-            v = h.min_v;
-            System.out.println("(" + v + "," + h.u + ")  "+  h.min_dist);
-            isInTree[v] = true;
-            for (int i = 0; i < numpoints; i++) {
-                // CHANGE?
-                if (isInTree[i] == false && c.weight(v,i) < h.dist[i] && c.weight(v,i) > 0) {
-                    h.insert(i, c.weight(v,i), v);
->>>>>>> 0509a6397a7b4b36040cd30694e4c72f5e054f7c
                 }
             }
         }
-
         long endTime = System.nanoTime();
         System.out.println((endTime-startTime)/1000000000.0);
 
